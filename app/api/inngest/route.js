7f6abf55e7f727420e930f6c7e6fb4883c61257c
Anup Gupta <anupg9643@gmail.com>
2025-08-16 12:57:13 +0530

    inngest put,credentail fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,7 +3,7 @@ import {inngest} from "@/lib/inngest/client";
 import { generateIndustryInsights, helloWorld } from "@/lib/inngest/functions";
 
 //create an api thtat servesmzero functions
-export const {GET,POST,PUT} =serve({
+export const {GET,POST} =serve({
     client:inngest,
     functions:[
         // your function will be passed here later
